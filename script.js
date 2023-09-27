96a041a7f85653824bfa453a32f3c0fd141dbe7b
Zarate <140767222+Ztnix@users.noreply.github.com>
2023-09-27 17:32:25 -0600

    Added RPS2

--- script.js
+++ script.js
@@ -6,57 +6,62 @@ let getComChoice = function(){
     return options[Math.floor(Math.random() * options.length)]; 
 }
 
-let getPlayerChoice = function(){
-    pChoice = prompt("Please select from: 'Rock', 'Paper', 'Scissors'",'Rock')
-    pChoice = pChoice.toLowerCase()
-    if (options.includes(pChoice)){
-        return pChoice;
-    }else{
-        alert("Wrong input, try again!")
-        getPlayerChoice();
-    }
- 
+let resetGame = function(){
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    contCpu=0;
+    contP=0;
+    score.textContent= `Player: ${contP}       Com: ${contCpu} !`
 }
 
-let playGame = function(){
+let rockBtn = document.getElementById("rockBtn")
+let paperBtn = document.getElementById("paperBtn")
+let scissorsBtn = document.getElementById("scissorsBtn")
+let resetBtn = document.getElementById("resetBtn")
+let result = document.getElementById("headtext")
+let scoreBoard = document.getElementById("score")
 
-    do {
-        let comChoice = getComChoice();
-        let playerChoice = getPlayerChoice();
-        console.log(`You selected: ${playerChoice}, Computer selected ${comChoice}`)
-        playRound(comChoice,playerChoice);
-    } while (contP <5 && contCpu <5);
+rockBtn.addEventListener("click",()=> startGame("rock"));
+paperBtn.addEventListener("click",()=> startGame("paper"));
+scissorsBtn.addEventListener("click",()=> startGame("scissors"));
+resetBtn.addEventListener("click",()=> resetGame())
 
-
+let startGame = function(playerChoice){
+    let comChoice = getComChoice();
+    result.textContent= `Player: ${playerChoice}      Computer: ${comChoice}`
+    playRound(comChoice,playerChoice);
 }
 
 let playRound = function(comChoice,playerChoice){
        
-        if (comChoice == playerChoice){
-            console.log("You Tied!")
-        }
-
-        else if (playerChoice == "rock" && comChoice == "scissors" ||
-                 playerChoice == "scissors" && comChoice == "paper"||
-                 playerChoice == "paper" && comChoice == "rock"){
-                    console.log("You Won!")
-                    contP++
-                 }
-        else{
-            console.log("You Lost!")
-            contCpu++
-        }    
-    
-    console.log(`You'r score: ${contP}, Computer's Score:${contCpu}`)
-
-    if (contP ==5){
-        alert("You Won The Game!")
-        console.log("You Won The Game!")
-    }
-    else if (contCpu == 5){
-        alert("You've Lost The Game!")
-        console.log("You Lost The Game!")
+    if (comChoice == playerChoice){
+        console.log("You Tied!")
     }
 
+    else if (playerChoice == "rock" && comChoice == "scissors" ||
+             playerChoice == "scissors" && comChoice == "paper"||
+             playerChoice == "paper" && comChoice == "rock"){
+                console.log("You Won!")
+                contP++
+             }
+    else{
+        console.log("You Lost!")
+        contCpu++
+    }    
+
+    score.textContent= `       Player: ${contP}              Computer: ${contCpu}`
+
+if (contP ==5){
+    alert("You Won The Game!")
+    console.log("You Won The Game!")
+    document.querySelectorAll(".gameButtons").forEach(e => e.setAttribute("disabled",true))
 }
-    playGame();
+else if (contCpu == 5){
+    alert("You've Lost The Game!")
+    console.log("You Lost The Game!")
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+}
